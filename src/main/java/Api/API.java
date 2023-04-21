2850d9543ee02f7b512e99ebe2f78dc875bbc293
Hexcss <cadersuay.javier@gmail.com>
2023-04-21 13:46:41 +0200

    Added Properties

--- src/main/java/Api/API.java
+++ src/main/java/Api/API.java
@@ -3,15 +3,29 @@ package Api;
 import DB.DataBase;
 import Utils.Downloader.DatFileDownloader;
 import Utils.Parser.CSVParser;
-import Utils.Printer.DataPrinter;
+import com.sun.tools.javac.Main;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Properties;
 
 public abstract class API {
     public static void setAirlines() {
-        String url = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat";
+        Properties properties = new Properties();
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input != null) {
+                properties.load(input);
+            } else {
+                throw new IOException("Configuration file 'config.properties' not found");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        String url = properties.getProperty("api.airline.endpoint");
 
         try {
             DatFileDownloader downloader = new DatFileDownloader();
